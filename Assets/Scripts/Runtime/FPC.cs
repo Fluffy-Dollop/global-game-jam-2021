c743a8d14ec91d749616f635c85f21e8bbde694d
zpallin <zpallin@gmail.com>
2021-01-28 21:16:08 -0800

    Separate out controllers so they are NetworkedBehaviors instead
    - NetworkedBehaviors unlike MonoBehaviors have the "isLocal" attribute
      that can be used to easily determine if an object can be controlled by
      a local player or not when it spawns.

--- Assets/Scripts/Runtime/FPC.cs
+++ Assets/Scripts/Runtime/FPC.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using MLAPI;
 
 //https://www.youtube.com/watch?v=vbILVirFV3A
 // Fird Person Controller
-public class FPC : MonoBehaviour
+public class FPC : NetworkedBehaviour
 {
     [SerializeField] Transform cam;
     [SerializeField] float speed = 10f;
     [SerializeField] float angularSpeed;
 
     CharacterController Controller;
+    NetworkedObject NetObj;
     Vector2 Move;
     float MouseX;
     float MouseY;
@@ -21,6 +23,7 @@ public class FPC : MonoBehaviour
     void Awake()
     {
         Controller = GetComponent<CharacterController>();
+        NetObj = GetComponent<NetworkedObject>();
 
         Transform cameraXform = this.gameObject.transform.GetChild(0); // camera
         cameraXform.position += Vector3.forward * -5.0f;
@@ -45,20 +48,23 @@ public class FPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float deltaTime = Time.deltaTime;
+        if (IsLocalPlayer)
+        {
+            float deltaTime = Time.deltaTime;
 
-        float angDeg = transform.localEulerAngles.y;
-        float angRad = angDeg * Mathf.PI / 180.0f;
-        float cos = Mathf.Cos(angRad);
-        float sin = Mathf.Sin(angRad);
+            float angDeg = transform.localEulerAngles.y;
+            float angRad = angDeg * Mathf.PI / 180.0f;
+            float cos = Mathf.Cos(angRad);
+            float sin = Mathf.Sin(angRad);
 
-        Vector3 relFwd = Move.y * cos * Vector3.forward + Move.y * sin * Vector3.right;
-        Vector3 relRgt = Move.x * cos * Vector3.right + Move.x * -sin * Vector3.forward;
-        Vector3 moveDir = (relFwd + relRgt).normalized;
-        Vector3 newMove = speed * deltaTime * moveDir;
+            Vector3 relFwd = Move.y * cos * Vector3.forward + Move.y * sin * Vector3.right;
+            Vector3 relRgt = Move.x * cos * Vector3.right + Move.x * -sin * Vector3.forward;
+            Vector3 moveDir = (relFwd + relRgt).normalized;
+            Vector3 newMove = speed * deltaTime * moveDir;
 
-        transform.Rotate(0, MouseX * rotateSpeed, 0);
+            transform.Rotate(0, MouseX * rotateSpeed, 0);
 
-        Controller.Move(newMove);
+            Controller.Move(newMove);
+        }
     }
 }
