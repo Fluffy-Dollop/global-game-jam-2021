df7f6131b89feea756beae36dfd7a94c45b86e83
Daniel McCue <daniel.mccue.ctr@tri.global>
2021-01-30 16:20:19 -0800

    attempt to get the shield to work with custom behavior

---
+++ Assets/Scripts/Runtime/ShieldItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Prototyping;
+
+public class ShieldItem : ItemBehavior
+{
+    public void Use()
+    {
+        Debug.Log("place holder: use ShieldItem");
+    }
+}
